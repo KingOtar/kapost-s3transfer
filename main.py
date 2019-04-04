75b20d074a511b996486d3266b01e11d3bb9f7a0
Samuel Lightsey <samuel.lightsey@MacBook-Pro-2.local>
2019-04-04 13:24:44 -0600

    Adding in boiler plate files for entire project

---
+++ main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Project for kapost s3 file transfer challange
+Refer to README for usage.
+"""
+
+__author__ = "Samuel Lightsey"
+__version__ = "0.1.0"
+__license__ = "MIT"
+
+
+def main():
+    print("hello world")
+
+
+if __name__ == "__main__":
+    main()
