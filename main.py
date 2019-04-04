90c4d3a1945ecf493efefee5080cdc7e0271335b
Samuel Lightsey <samuel.lightsey@MacBook-Pro-2.local>
2019-04-04 13:41:34 -0600

    Adding argument handling, validation and help using argparse

--- main.py
+++ main.py
@@ -8,10 +8,20 @@ __author__ = "Samuel Lightsey"
 __version__ = "0.1.0"
 __license__ = "MIT"
 
+import argparse
+import sys
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', "--size",  help='Use parsable size units according to "man units"')
+    parser.add_argument('bucket1', action="store")
+    parser.add_argument('bucket2', action="store")
+    args = parser.parse_args()
+    return args
 
 def main():
     print("hello world")
 
-
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    arguments = parse_arguments()
+    main(*arguments)
