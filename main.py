0844abefd5968390b2ab0cee391122aec491df87
Samuel Lightsey <samuel.lightsey@MacBook-Pro-2.local>
2019-04-04 14:07:24 -0600

    add human friendly lib to parse size

--- main.py
+++ main.py
@@ -10,18 +10,24 @@ __license__ = "MIT"
 
 import argparse
 import sys
+import humanfriendly
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-s', "--size",  help='Use parsable size units according to "man units"')
+    parser.add_argument('-s', "--size",  help='Use size units accordingly. ex. 5MB, 200GB 1TB', required=True)
     parser.add_argument('bucket1', action="store")
     parser.add_argument('bucket2', action="store")
     args = parser.parse_args()
     return args
 
-def main():
-    print("hello world")
+def main(args):
+    # Make it easier to read the args passed to script. All required by argparse
+    bucket1 = args.bucket1
+    bucket2= args.bucket2
+    
+    size = num_bytes = humanfriendly.parse_size(args.size);
+    print(size)
 
 if __name__ == '__main__':
     arguments = parse_arguments()
-    main(*arguments)
+    main(arguments)
